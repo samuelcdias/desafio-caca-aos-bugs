1db7a2d124f21c472c623d76c7f889a71aaf9d65
Bernardo <bemeine15@hotmail.com>
2024-10-28 15:06:46 -0300

    Refactored user tests to reduce user.ShouldAuthenticate calls

--- unit-tests/Balta.Domain.Test/AccountContext/Entities/UserTests.cs
+++ unit-tests/Balta.Domain.Test/AccountContext/Entities/UserTests.cs
@@ -9,80 +9,83 @@ namespace Balta.Domain.Test.AccountContext.Entities;
 
 public class UserTests
 {
-    private const string EmailAddress = "test@test.com"; 
+    private const string EmailAddress = "test@test.com";
     private const string PlainTextPassword = "senhabemlouca123";
     private readonly Mock<IDateTimeProvider> _dateTimeProvider = new();
-    
+
     [Fact]
     public void ShouldCreateUserIfEmailAndPasswordIsValid()
     {
         var email = Email.ShouldCreate(EmailAddress, _dateTimeProvider.Object);
         var password = Password.ShouldCreate(PlainTextPassword);
         var user = User.ShouldCreate(email, password);
-        
+
         Assert.NotNull(user);
-        Assert.Equal(email, user.Email);         
-        Assert.Equal(password, user.Password);   
-        Assert.Null(user.LockOut);               
-        Assert.Equal(0, user.AccessFailedCount); 
+        Assert.Equal(email, user.Email);
+        Assert.Equal(password, user.Password);
+        Assert.Null(user.LockOut);
+        Assert.Equal(0, user.AccessFailedCount);
     }
 
     [Fact]
     public void ShouldIncrementAccessFailedCountIfAuthFails()
     {
-        
+
         var email = Email.ShouldCreate(EmailAddress, _dateTimeProvider.Object);
         var password = Password.ShouldCreate(PlainTextPassword);
         var user = User.ShouldCreate(email, password);
-        
+
         user.ShouldAuthenticate();
-        
+
         Assert.Equal(1, user.AccessFailedCount);
     }
 
-    [Fact]
-    public void ShouldBLockUserIfMultipleFailAttempts()
+    [Theory]
+    [InlineData(4)] // Número de tentativas para bloquear o usuário
+    public void ShouldBlockUserIfMultipleFailAttempts(int attempts)
     {
         var email = Email.ShouldCreate(EmailAddress, _dateTimeProvider.Object);
         var password = Password.ShouldCreate(PlainTextPassword);
         var user = User.ShouldCreate(email, password);
-        
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
+
+        for (int i = 0; i < attempts; i++)
+        {
+            user.ShouldAuthenticate();
+        }
 
         Assert.NotNull(user.LockOut);
     }
-    
-    [Fact]
-    public void ShouldSendBlockingMessageIfMultipleFailAttempts()
+
+    [Theory]
+    [InlineData(4)]
+    public void ShouldSendBlockingMessageIfMultipleFailAttempts(int attempts)
     {
         var email = Email.ShouldCreate(EmailAddress, _dateTimeProvider.Object);
         var password = Password.ShouldCreate(PlainTextPassword);
         var user = User.ShouldCreate(email, password);
-        
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
+
+        for (int i = 0; i < attempts; i++)
+        {
+            user.ShouldAuthenticate();
+        }
 
         Assert.Equal(UserErrors.LockedOutByLoginAttemptsMessage, user.LockOut?.LockOutReason);
     }
-    
-    [Fact]
-    public void ShouldNotIncrementAccessFailedCountAfterBlock()
+
+    [Theory]
+    [InlineData(4)]
+    public void ShouldNotIncrementAccessFailedCountAfterBlock(int attempts)
     {
         var email = Email.ShouldCreate(EmailAddress, _dateTimeProvider.Object);
         var password = Password.ShouldCreate(PlainTextPassword);
         var user = User.ShouldCreate(email, password);
-        
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
-        user.ShouldAuthenticate();
+
+        for (int i = 0; i < attempts; i++)
+        {
+            user.ShouldAuthenticate();
+        }
 
         Assert.True(user.AccessFailedCount <= 3);
     }
-    
+
 }
